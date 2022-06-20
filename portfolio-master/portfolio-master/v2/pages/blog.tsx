644177537eb72688dc9fc94118e56150c0401abf
nikhilsaraswat <36073095+nikhilsaraswat@users.noreply.github.com>
2022-06-20 15:20:46 +0100

    Update blog.tsx

--- portfolio-master/portfolio-master/v2/pages/blog.tsx
+++ portfolio-master/portfolio-master/v2/pages/blog.tsx
@@ -29,7 +29,7 @@ const Blog = () => {
                     </div>
                     </button>
                     </Link>
-                <Link href="/blogs/marriotthotel">
+                <Link href="/blogs/webscrapping">
                 <button className="col-span-1 row-span-1 border-2 grid grid-cols-2 m-2">
                     <div className="col-span-1 grid grid-rows-6">
                         <div className="row-span-4">
@@ -41,11 +41,11 @@ const Blog = () => {
                         placeholder="blur"
                     />
                         </div>
-                        <h4 className="row-span-1 grid place-content-center">Marriott Hotel-Data Breach</h4>
-                        <p className="row-span-1 grid place-content-center">Created on 18-06-2022</p>
+                        <h4 className="row-span-1 grid place-content-center">Web Scrapping-Amazon</h4>
+                        <p className="row-span-1 grid place-content-center">Created on 20-06-2022</p>
                     </div>
                     <div className="col-span-1 m-2">
-                        <p>Hello, This is my first blog in which i am going to explain about the data breach scenario of The Giant Marriott Hotel happened in 2018.</p>
+                        <p>Web Scrapping bot to scrap data from Amazon website at regular interval and will send email on matching of criterion.</p>
                     </div>
                     </button>
                     </Link>
