45f0e6d27b3d2d649ca31482f27cda079e25aebe
nikhilsaraswat <36073095+nikhilsaraswat@users.noreply.github.com>
2022-06-20 21:05:16 +0100

    Update webscrapping.tsx

--- portfolio-master/portfolio-master/v2/pages/blogs/webscrapping.tsx
+++ portfolio-master/portfolio-master/v2/pages/blogs/webscrapping.tsx
@@ -21,9 +21,8 @@ const WebScrapping = () => {
           <p >
             Web scraping is the art of extracting data from the web. It is used to obtain any kind of data from any website. Here, I am using it to extract data from Amazon Website. I will be using Colab Notebooks and Python3. The purpose is to create a web scraper bot that will be able to scrape data from the amazon website at regular intervals and will be able to email on the matching of certain criteria. I will be using libraries such as Pandas for managing data, BeautifulSoup for extracting data, and SMTP for sending an email.  
           </p>
-                <a href="https://github.com/nikhilsaraswat/bot">Check the code here.</a>
-            </div>
-            <div className="grid justify-center m-2">
+                <a href="https://github.com/nikhilsaraswat/bot" className="text-blue-600">Check the code here.</a>
+                 <div className="grid justify-center m-2">
             <h1 className="text-5xl font-serif mb-2">Excel Sheet with the data.</h1>
           <Image
                         src={excel}
@@ -41,6 +40,8 @@ const WebScrapping = () => {
                         className="w-max h-max m-2"
                         placeholder="blur"
                     /></div>
+            </div>
+           
             
             <div className="col-span-2"></div>
             
